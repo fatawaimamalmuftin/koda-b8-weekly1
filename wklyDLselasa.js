f83908db66e6e9a34ce6de2c4ba0cc3be3f10001
XONE <muftinmarmut@gmail.com>
2026-05-14 22:47:24 +0700

    Add files via upload

--- wklyDLselasa.js
+++ wklyDLselasa.js
@@ -46,52 +46,52 @@ rl.question("Masukan Nama Anda : ", function (nama) {
         switch (checkOut) {
           case "1":
             CO.push[`1. ${Kmenu[0].minuman} - Rp. ${Kmenu[0].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Kopi Hear masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "2":
             CO.push[`2. ${Kmenu[1].minuman} - Rp. ${Kmenu[1].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Cafe Latte masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "3":
             CO.push[`3. ${Kmenu[2].minuman} - Rp. ${Kmenu[2].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Cappuccino masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "4":
             CO.push[`4. ${Kmenu[3].minuman} - Rp. ${Kmenu[3].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Espresso masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "5":
             CO.push[`5. ${Kmenu[4].minuman} - Rp. ${Kmenu[4].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Americano masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "6":
             CO.push[`6. ${Kmenu[5].minuman} - Rp. ${Kmenu[5].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Signature V60 masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "7":
             CO.push[`7. ${Kmenu[6].minuman} - Rp. ${Kmenu[6].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Vietnam Drip masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "8":
             CO.push[`8. ${Kmenu[7].minuman} - Rp. ${Kmenu[7].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Espresso BOOM~! masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "9":
             CO.push[`9. ${Kmenu[8].minuman} - Rp. ${Kmenu[8].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Kopi Gula Aren masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           case "10":
             CO.push[`10. ${Kmenu[9].minuman} - Rp. ${Kmenu[9].harga}`];
-            console.log(`pesanan anda sudah masuk keranjang : ${CO}`);
+            console.log(`Machiato masuk keranjang : ${CO}`);
             ulangMenu();
             break;
           default:
